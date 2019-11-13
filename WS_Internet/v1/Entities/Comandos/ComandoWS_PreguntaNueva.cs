58adc716489cba62df94a381cc75d5e7f313f373
Marcos Bonetto <sasia_r@cordoba.gov.ar>
2019-11-13 15:05:58 -0300

    a

---
+++ WS_Internet/v1/Entities/Comandos/ComandoWS_PreguntaNueva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace WS_Internet.v1.Entities.Comandos
+{
+    [Serializable]
+    public class ComandoWS_PreguntaNueva
+    {
+        public string Titulo { get; set; }
+        public int? Aplicacion { get; set; }
+        public int? Tema { get; set; }
+        public string Descripcion { get; set; }
+        public string Tags { get; set; }
+        public int? IdUsuario { get; set; }
+
+        
+    }
+}
